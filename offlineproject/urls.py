99ad15f002ad3403616822abe899e1cb1025b593
hildaAbigaba <abigabahilda@gmail.com>
2025-07-05 15:16:44 +0300

    Added html pages to systems and Home apps

--- offlineproject/urls.py
+++ offlineproject/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Home.views import Homepage
+from Systems.views import Systemspage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',Homepage),
+    path('systems/', Systemspage)
 ]
